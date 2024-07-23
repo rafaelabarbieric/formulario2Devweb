36a731e0c92b5ff47eb383c838e72e1ec0b363fc
rafelabarbieric <rafaelabarbieric@gmail.com>
2024-07-23 16:56:52 -0300

    feat: edição do form

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 
-import { ref } from 'vue'
+//#655BA6 color
+
+import { reactive } from 'vue'
 import Formulario from '@/components/Formulario.vue'
 import Perfil from '@/components/Perfil.vue'
 
-const user = ref([
+const user = reactive([
   {nome: ''},
   {email: ''},
   {senha: ''},
@@ -18,43 +20,13 @@ const user = ref([
   {biografia: ''},
 ])
 
-let estados = [
-  {name: 'AC'},
-  {name: 'AL'},
-  {name: 'AP'}, 
-  {name: 'AM'}, 
-  {name: 'BA'}, 
-  {name: 'CE'}, 
-  {name: 'DF'},
-  {name: 'ES'}, 
-  {name: 'GO'}, 
-  {name: 'MA'}, 
-  {name: 'MT'}, 
-  {name: 'MS'}, 
-  {name: 'MG'}, 
-  {name: 'PA'}, 
-  {name: 'PB'}, 
-  {name: 'PR'}, 
-  {name: 'PE'}, 
-  {name: 'PI'}, 
-  {name: 'RJ'}, 
-  {name: 'RN'},
-  {name: 'RS'}, 
-  {name: 'RO'}, 
-  {name: 'RR'}, 
-  {name: 'SC'}, 
-  {name: 'SP'}, 
-  {name: 'SE'},
-  {name: 'TO'}
-  ]
-
 </script>
 
 <template>
   
-  <Formulario :dados="user" :nome-estados="estados"></Formulario>
+  <Formulario :dados="user"></Formulario>
 
-  <Perfil :dados-perfil="user"></Perfil>
+  <Perfil :dados="user"></Perfil>
 
 </template>
 
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,6 +1,36 @@
 <script setup>
 
-defineProps(['dados', 'nomeEstados'])
+defineProps(['dados'])
+
+let estados = [
+  {name: 'AC'},
+  {name: 'AL'},
+  {name: 'AP'}, 
+  {name: 'AM'}, 
+  {name: 'BA'}, 
+  {name: 'CE'}, 
+  {name: 'DF'},
+  {name: 'ES'}, 
+  {name: 'GO'}, 
+  {name: 'MA'}, 
+  {name: 'MT'}, 
+  {name: 'MS'}, 
+  {name: 'MG'}, 
+  {name: 'PA'}, 
+  {name: 'PB'}, 
+  {name: 'PR'}, 
+  {name: 'PE'}, 
+  {name: 'PI'}, 
+  {name: 'RJ'}, 
+  {name: 'RN'},
+  {name: 'RS'}, 
+  {name: 'RO'}, 
+  {name: 'RR'}, 
+  {name: 'SC'}, 
+  {name: 'SP'}, 
+  {name: 'SE'},
+  {name: 'TO'}
+  ]
 
 </script>
 
@@ -24,12 +54,33 @@ defineProps(['dados', 'nomeEstados'])
 
             <div>
                 <label for="dataNasc">Data de Nascimento:</label>
-                <input id="dataNasc" type="date" v-model="dataNasc.nome">
+                <input id="dataNasc" type="date" v-model="dados.dataNasc">
+            </div>
+
+            <div>
+                <label for="endereco">Endereço</label>
+                <input id="endereco" type="text" v-model="dados.endereco">
             </div>
 
             <div>
                 <label for="cidade">Cidade:</label>
-                <input id="cidade" type="text" v-model="cidade.nome">
+                <input id="cidade" type="text" v-model="dados.cidade">
+            </div>
+
+            <div>
+                <label for="estado">Estado:</label>
+                <select name="estados" id="estado" v-model="dados.estados">
+                    <option v-for="estado of estados " :key="estado" value="estado.name">{{ estado.name }}</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="hobbies">Hobbies:</label>
+                <input type="checkbox" v-model="dados.hobbies" id="esportes" value="esportes"> Esportes
+                <input type="checkbox" v-model="dados.hobbies" id="musica" value="musicas"> Músicas
+                <input type="checkbox" v-model="dados.hobbies" id="filme" value="filmes"> Filmes
+                <input type="checkbox" v-model="dados.hobbies" id="viagens" value="viagens"> Viagens
+                <input type="checkbox" v-model="dados.hobbies" id="leitura" value="leitura"> Leitura
             </div>
 
             <div>
@@ -39,7 +90,7 @@ defineProps(['dados', 'nomeEstados'])
 
             <div>
                 <label for="cSenha">Confirmação de senha:</label>
-                <input id="cSenha" type="password" v-model="cSenha.nome">
+                <input id="cSenha" type="password" v-model="dados.cSenha">
             </div>
 
         </form>
